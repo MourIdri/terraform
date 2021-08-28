8be8c93580837303ebac66a0deb2154d5506ebae
chapeau <mou.idri@gmail.com>
2021-08-28 14:37:12 +0200

    Update main.tf

--- hub/main.tf
+++ hub/main.tf
@@ -1,17 +1,17 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.70"
-    }
-  }
+# terraform {
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "~> 2.70"
+#    }
+#  }
 
   # required_version = ">= 0.14.9"
-}
+# }
 
 provider "azurerm" {
-
+  version = "~> 2.70"
   features {
     key_vault {
       purge_soft_delete_on_destroy = true
